04c779714271f6a8b6f27745a085262f64f4bfa7
Iqra Abbaali <iqra.abbaali@gmail.com>
2020-02-01 10:52:55 -0800

    implemented sign in and sign up

--- PetrTradr/LogInViewController.swift
+++ PetrTradr/LogInViewController.swift
@@ -7,20 +7,51 @@
 //
 
 import UIKit
+import Parse
 
 private let reuseIdentifier = "Cell"
 
 class LogInViewController: UICollectionViewController {
 
     @IBOutlet weak var usernameField: UITextField!
-    
-    
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
-    @IBAction func onSignIn(_ sender: Any) {
+    
+    @IBAction func onSignIn(_ sender: Any)
+    {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil
+            {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else
+            {
+                self.errorLabel.text = "Sorry, the password you entered is incorrect."
+            }
+            
+        }
     }
     
-    @IBAction func onSignUp(_ sender: Any) {
+    @IBAction func onSignUp(_ sender: Any)
+    {
+        let user = PFUser(className: "_User")
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if (success)
+            {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else
+            {
+                self.errorLabel.text = "Sorry that username is already taken."
+            }
+        }
     }
     
     override func viewDidLoad() {
