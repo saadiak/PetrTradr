c8bb6d266b84e6d469c44f2f4a584a6f80254c26
saadiak <saadiak@uci.edu>
2020-02-02 02:25:07 -0800

    Added message field

---
+++ PetrTradr/ContactViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContactViewController.swift
+//  PetrTradr
+//
+//  Created by Saadia Karim on 2/2/20.
+//  Copyright © 2020 Safiyah Lakhany. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendMessage(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
